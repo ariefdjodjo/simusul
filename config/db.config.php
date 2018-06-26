b633cf4e49a73158967b2f7e0e553e0b4b2fce15
Arief Gunawan <arief_djodjo@yahoo.com>
2018-06-26 16:07:25 +0700

    Update db.config.php

--- config/db.config.php
+++ config/db.config.php
@@ -2,7 +2,7 @@
 	session_start();
 
 	//setting database
-	$host		= "db4free.net:3306";
+	$host		= "85.10.205.173:3306";
 	$userdb 	= "ariefdjodjo";
 	$passdb 	= "4r13fgun4w4n";
 	$database 	= "simusul";
