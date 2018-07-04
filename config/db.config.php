4dc8353980cc07973405ccfbde00bb97462f1a65
Arief Gunawan <arief_djodjo@yahoo.com>
2018-07-04 10:01:53 +0700

    Update db.config.php

--- config/db.config.php
+++ config/db.config.php
@@ -2,7 +2,7 @@
 	session_start();
 
 	//setting database
-	$host		= "http://185.27.134.10/";
+	$host		= "sql113.byethost.com";
 	$userdb 	= "b15_20757151";
 	$passdb 	= "ariefgunawan";
 	$database 	= "b15_20757151_proyek_web";
